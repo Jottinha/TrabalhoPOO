7fba40d3f8cc48cbe8d4124f983d49605b4f4d0f
João Vitor <69482936+Jottinha@users.noreply.github.com>
2023-03-13 21:07:13 -0300

    Add files via upload

---
+++ Aula_13_03_23/src/aula_13_03_23/Aula_13_03_23.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula_13_03_23;
+
+/**
+ *
+ * @author LABORATÓRIO
+ */
+public class Aula_13_03_23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home telaHome = new Home();
+        telaHome.setVisible(true);
+    }
+    
+}
